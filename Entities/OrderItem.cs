fbb5a7b8461a17505cb5eacbba0ad72e574c86ec
William Gaertner <william_nefi@hotmail.com>
2019-08-28 23:57:23 -0400

    projeto enumeracao e composicao

---
+++ Entities/OrderItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumeracaoComposicao.Entities
+{
+    class OrderItem
+    {
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public Product Product { get; set; }
+
+        public OrderItem()
+        {
+
+        }
+
+        public OrderItem(int quantity, double price, Product product)
+        {
+            Quantity = quantity;
+            Price = price;
+            Product = product;
+        }
+
+        public double SubTotal()
+        {
+            return Quantity * Price;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnumeracaoComposicao.Entities.Enum;
+using EnumeracaoComposicao.Entities;
+
+namespace EnumeracaoComposicao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Enter client data:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Brith data (DD/MM/YYYY): ");
+            DateTime date = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Enter order data: ");
+            Console.Write("Status: ");
+            //OrderStatus status = Enum.Parse <OrderStatus>(Console.ReadLine());// core
+            Enum.TryParse<OrderStatus>(Console.ReadLine(), out OrderStatus status); //.Net
+
+            Client client = new Client(name, email, date);
+            Order order = new Order(DateTime.Now, status, client);
+
+            Console.Write("How many items to this order: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter item #{i} data: ");
+                Console.Write("Product name: ");
+                string prodname = Console.ReadLine();
+                Console.Write("Produto price: ");
+                double price = double.Parse(Console.ReadLine());
+
+                Product product = new Product(prodname, price);
+
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());                
+
+                OrderItem orderItem = new OrderItem(quantity, price, product);
+                order.AddItem(orderItem);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("ORDER SUMMARY:");
+            Console.WriteLine(order);
+            Console.ReadLine();
+        }
+    }
+}
